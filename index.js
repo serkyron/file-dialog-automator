9575307649d5ccf0893d7f1197aa107324fd4e13
Sergei Krivosheenko <serkyron@gmail.com>
2020-09-19 13:23:04 +0300

    initial

---
+++ index.js
@@ -0,0 +1,27 @@
+const { execFile } = require('child_process');
+const os = require('os');
+
+module.exports.openFile = function(fullPath) {
+    return new Promise((resolve, reject) => {
+        if (!fullPath) {
+            reject(new Error("Invalid path"));
+            return;
+        }
+
+        let script = "./node_modules/file-dialog-automation/open.sh";
+
+        if (os.platform() === "win32") {
+            // todo: implement for windows
+            script = null;
+        }
+
+        execFile(script, [fullPath], {windowsHide: true}, (error, stdout, stderr) => {
+            if (error) {
+                reject(error);
+                return;
+            }
+
+            resolve();
+        });
+    });
+};
---
+++ open.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file=$1
+browser="chrome"
+win_name="Open File"
+
+# Find window PID
+WIN=$(for pid in $(xdotool search --class "$browser"); do if [[ $(xdotool getwindowname $pid) == "$win_name" ]]; then echo $pid; fi; done)
+
+# Switch to the window
+xdotool windowactivate $WIN
+sleep 3
+
+# Send file path
+xdotool type --delay=500 "$file"
+sleep 3
+
+# Press "Open" button
+xdotool key alt+o
